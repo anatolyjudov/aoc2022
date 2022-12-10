0af3a7e33195c406de1bd9756a6ef79081791ca4
Anatoliy Yudov <anatoliy.yudov@supermetrics.com>
2022-12-10 08:27:46 +0200

    minor simplifications

--- 10/aoc-2022-10.php
+++ 10/aoc-2022-10.php
@@ -4,36 +4,27 @@ declare(strict_types = 1);
 
 $fp = fopen('input.txt', 'r');
 
-$cycle = 1;
-$x     = 1;
-
-$resultsCycles = [20, 60, 100, 140, 180, 220];
+$resultsCycles = range(20, 220, 40);
 $signal        = 0;
+
+$cycle = $x = 1;
 
 while($line = fgets($fp)) {
     $line = rtrim($line);
 
+    $diffX = 0;
     if ($line === 'noop') {
         $loop = 1;
-        $diffX = 0;
     } elseif (substr($line, 0, 4) === 'addx') {
         $loop = 2;
         $diffX = (int) substr($line, 5);
-    } else {
-        die('unknown line: ' . $line);
     }
 
     for($c = 0; $c < $loop; $c++) {
 
         $drawPos = ($cycle - 1) % 40;
-        if ($drawPos >= ($x - 1) && $drawPos <= ($x + 1)) {
-            echo '#';
-        } else {
-            echo '.';
-        }
-        if ($drawPos === 39) {
-            echo "\r\n";
-        }
+        echo ($drawPos >= ($x - 1) && $drawPos <= ($x + 1)) ? '#' : '.';
+        if ($drawPos === 39) echo "\r\n";
 
         $cycle++;
 
