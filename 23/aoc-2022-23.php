e6f5568822d6451d0924f32db98fd95451f9a8ae
Anatoliy Yudov <anatoliy.yudov@supermetrics.com>
2022-12-27 11:13:08 +0200

    compact refactoring

--- 23/aoc-2022-23.php
+++ 23/aoc-2022-23.php
@@ -25,65 +25,46 @@ foreach(explode(PHP_EOL, file_get_contents('input.txt')) as $row) {
 
 $needToRun = true;
 $strategies = [[N, NE, NW], [S, SE, SW], [W, NW, SW], [E, NE, SE]];
-$tenthRoundResult = 0;
-$nobodyMoves = false;
-$round = 0;
-while($round < 100000 && (!$nobodyMoves)) {
-    $round++;
+$round = $tenthRoundResult = 0;
+do {
     $mapOfElves = getMapOfElves($elves);
-
-    $decisions = [];
-    // loop over elves
-    foreach ($elves as $e => $elf) {
+    $decisions = $newPositions = [];
+    foreach ($elves as $e => $elf) { // loop over elves
         $elfAround = [];
-        foreach([N, NE, E, SE, S, SW, W, NW] as $test) {
-            if (isset($mapOfElves[$elf[1] + $test[1]][$elf[0] + $test[0]])) {
+        foreach([N, NE, E, SE, S, SW, W, NW] as $test)
+            if (isset($mapOfElves[$elf[1] + $test[1]][$elf[0] + $test[0]]))
                 $elfAround[$test[1]][$test[0]] = $mapOfElves[$elf[1] + $test[1]][$elf[0] + $test[0]];
-            }
-        }
-        if (sizeof($elfAround) === 0) {
-            // nobody around, we can stay
-            continue;
-        }
-        // loop over strategies
-        for($s = 0; $s < sizeof($strategies); $s++) {
-            // test strategy - loop over its test directions
-            foreach($strategies[$s] as $test) {
-                if (isset($elfAround[$test[1]][$test[0]])) {
-                    continue 2; // something is there, next strategy
-                }
-            }
-            // strategy ok
-            $decisions[$e] = $strategies[$s][0];
+
+        if (sizeof($elfAround) === 0) continue; // nobody around, we can stay
+
+        for($s = 0; $s < sizeof($strategies); $s++) {              // loop over strategies
+            foreach($strategies[$s] as $test)                          // loop over strategy's test directions
+                if (isset($elfAround[$test[1]][$test[0]])) continue 2; // something is there, next strategy
+
+            $decisions[$e] = $strategies[$s][0];                       // strategy ok
             break;
         }
     }
 
-    // moves
-    $newPositions = [];
-    foreach ($decisions as $e => $decision) {
+    foreach ($decisions as $e => $decision)
         $newPositions[$elves[$e][1] + $decision[1]][$elves[$e][0] + $decision[0]][] = $e;
-    }
+
     $nobodyMoves = true;
     foreach ($newPositions as $y => $row) {
         foreach ($row as $x => $posInfo) {
             if (sizeof($posInfo) > 1) continue;
-            // elf is moving
             $elfNum = $posInfo[0];
-            $elves[$elfNum][0] += $decisions[$elfNum][0];
+            $elves[$elfNum][0] += $decisions[$elfNum][0]; // elf is moving
             $elves[$elfNum][1] += $decisions[$elfNum][1];
             $nobodyMoves = false;
         }
     }
 
-    // final part of the round - shifting strategies
-    $strategies[] = array_shift($strategies);
+    $strategies[] = array_shift($strategies); // shifting strategies
 
-    // check if it's a 10th round
-    if ($round === 10) {
-        printf('First star: %d%s', getCount($elves), PHP_EOL);
-    }
-}
+    if ($round === 10) printf('First star: %d%s', getCount($elves), PHP_EOL); // first star value
+
+} while ($round++ < 100000 && (!$nobodyMoves));
 
 printf('Second star: %d%s', $round, PHP_EOL);
 
@@ -108,9 +89,7 @@ function getRectangle(array &$elves): array
 function getMapOfElves(array &$elves): array
 {
     $map = [];
-    foreach($elves as $n => $elf) {
-        $map[$elf[1]][$elf[0]] = $n;
-    }
+    foreach($elves as $n => $elf) $map[$elf[1]][$elf[0]] = $n;
     return $map;
 }
 
@@ -120,11 +99,7 @@ function printElves(array &$elves)
     $mapOfElves = getMapOfElves($elves);
     for($y = $mins[1]; $y <= $maxs[1]; $y++) {
         for($x = $mins[0]; $x <= $maxs[0]; $x++) {
-            if (!isset($mapOfElves[$y][$x])) {
-                echo '.';
-            } else {
-                echo '#';
-            }
+            echo (isset($mapOfElves[$y][$x])) ? '#' : '.';
         }
         echo PHP_EOL;
     }
